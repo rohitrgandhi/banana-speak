abcc63f5d9d6635a774a7ca49bf9a65f554a14db
Rohit Gandhi <rohit.r.gandhi@gmail.com>
2021-08-11 23:39:15 +0530

    input output processing done

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var btnTranslate= document.querySelector("#btn-translate");
 
 var txtInput=document.querySelector("#txt-input");
 
+var output=document.querySelector("#output");
+
 function handleClickEvent()
 {
-    console.log("Clicked!");
-    console.log(txtInput.value);
+    output.innerText="banana "+txtInput.value;
 }
 btnTranslate.addEventListener("click" , handleClickEvent);
